d62858fe1751928e6675176914708ecb252684de
freightduck <xilverfang12@gmail.com>
2025-04-13 10:47:37 +0100

    fallback

--- index.js
+++ index.js
@@ -101,5 +101,5 @@ app.post('/submit', async (req, res) => {
 
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+    console.log(`Server is running on port and some server ${process.env.PORT}`);
 })
